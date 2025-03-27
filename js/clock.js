7771ef858639b7496581a7432827c7e33f9d41d8
Jonas <92853498+joraka@users.noreply.github.com>
2025-03-27 14:39:24 +0200

    reverse countdown support

--- js/clock.js
+++ js/clock.js
@@ -5,13 +5,15 @@ const [daysEl, hoursEl, minutesEl, secondsEl] = Array.from(
 const targetTime = new Date("2025-07-21T15:30:00");
 
 function updateCountdown() {
-  const diffInSeconds = Math.abs(Date.now() - targetTime) / 1000;
+  const diff = targetTime - Date.now();
+  const diffInSeconds = Math.abs(diff) / 1000;
   const days = Math.floor(diffInSeconds / 60 / 60 / 24);
   const hours = Math.floor((diffInSeconds / 60 / 60) % 24);
   const minutes = Math.floor((diffInSeconds / 60) % 60);
   const seconds = Math.floor(diffInSeconds % 60);
 
-  if (daysEl.innerText != days) daysEl.innerText = days;
+  const daysStr = diff < 0 ? `-${days}` : days;
+  if (daysEl.innerText != daysStr) daysEl.innerText = daysStr;
   if (hoursEl.innerText != hours) hoursEl.innerText = hours;
   if (minutesEl.innerText != minutes) minutesEl.innerText = minutes;
   if (secondsEl.innerText != seconds) secondsEl.innerText = seconds;
